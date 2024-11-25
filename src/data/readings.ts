fc9396566c9d4a942ae584fdc72443f4fad01b21
Michael Lohier <lohiermichael@gmail.com>
2024-11-25 17:28:08 +0200

    Put real reading values
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- src/data/readings.ts
+++ src/data/readings.ts
@@ -1,12 +1,12 @@
 export const readings = [
   { date: '16/11/2024', value: 2835.2, consumption: 0 },
-  { date: '17/11/2024', value: 2841.8, consumption: 6.6 },
-  { date: '18/11/2024', value: 2847.9, consumption: 6.1 },
-  { date: '19/11/2024', value: 2854.2, consumption: 6.3 },
-  { date: '20/11/2024', value: 2860.7, consumption: 6.5 },
-  { date: '21/11/2024', value: 2866.9, consumption: 6.2 },
-  { date: '22/11/2024', value: 2873.1, consumption: 6.2 },
-  { date: '23/11/2024', value: 2879.4, consumption: 6.3 },
-  { date: '24/11/2024', value: 2885.8, consumption: 6.4 },
-  { date: '25/11/2024', value: 2891.0, consumption: 5.2 }
+  { date: '17/11/2024', value: 2850.33, consumption: 15.13 },
+  { date: '18/11/2024', value: 2854.9, consumption: 4.63 },
+  { date: '19/11/2024', value: 2860.2, consumption: 5.3 },
+  { date: '20/11/2024', value: 2864.7, consumption: 4.5 },
+  { date: '21/11/2024', value: 2868.9, consumption: 4.2 },
+  { date: '22/11/2024', value: 2873.1, consumption: 4.2 },
+  { date: '23/11/2024', value: 2877.4, consumption: 4.3 },
+  { date: '24/11/2024', value: 2883.8, consumption: 6.4 },
+  { date: '25/11/2024', value: 2891.0, consumption: 7.2 }
 ];
